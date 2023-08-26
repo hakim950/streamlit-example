620162f8b7ccac3538ea6938ecfeeb2e23f8dac0
hakim950 <61159205+hakim950@users.noreply.github.com>
2023-08-26 19:48:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,77 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import instaloader
+import time
+import pandas as pd
+import requests
+import os
 
-"""
-# Welcome to Streamlit!
+# Initialize instaloader
+loader = instaloader.Instaloader()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def get_instagram_data(account_name):
+    try:
+        profile = instaloader.Profile.from_username(loader.context, account_name)
+    except Exception as e:
+        st.write(f"Error fetching profile: {e}")
+        return None
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
     data = []
 
-    points_per_turn = total_points / num_turns
+    try:
+        posts = profile.get_posts()
+    except Exception as e:
+        st.write(f"Error fetching posts: {e}")
+        return None
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    for post in posts:
+        time.sleep(0.5)  # Increasing the delay to 5 seconds
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        post_data = {
+            'Date': post.date.strftime('%Y-%m-%d %H:%M:%S'),
+            'Media': post.url,
+            'Caption': post.caption,
+            'Likes': post.likes, 
+            'Comments': post.comments,
+            'Type': '',
+            'PostURL': f"https://www.instagram.com/p/{post.shortcode}"
+        }
+        
+        if post.typename == 'GraphVideo':
+            post_data['Type'] = 'video post'
+        elif post.typename == 'GraphImage':
+            post_data['Type'] = 'post'
+        
+        data.append(post_data)
+        
+    df = pd.DataFrame(data)
+    return df
+
+def main():
+    st.title('Instagram Data Fetcher')
+
+    accounts = st.text_area("Enter Instagram account names (comma-separated)").split(',')
+    accounts = [account.strip() for account in accounts]
+
+    if st.button('Submit'):
+        if not accounts:
+            st.write("Please enter Instagram account names.")
+        else:
+            for account_name in accounts:
+                with st.expander(account_name):  # Each account will be a separate tab
+                    df = get_instagram_data(account_name)
+
+                    if df is not None:
+                        for idx, row in df.iterrows():
+                            st.markdown(f"### Date: {row['Date']}")
+                            st.markdown(f'<iframe src="{row["PostURL"]}/embed" width="480" height="480" frameborder="0" scrolling="no" allowtransparency="true"></iframe>', unsafe_allow_html=True)
+
+                            # Display the data as a table
+                            st.table(pd.DataFrame({
+                                'Caption': [row['Caption']],
+                                'Likes': [row['Likes']],
+                                'Comments': [row['Comments']],
+                                'Type': [row['Type']]
+                            }))
+
+if __name__ == '__main__':
+    main()
